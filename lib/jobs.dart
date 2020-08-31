e75453572644fe27fd18b61cc22816f906de3bf8
Gregorius Agung Narindra Aditantyo <narindra.aditantyo@gmail.com>
2020-08-31 21:00:15 +0700

    [WIP] Home page with list view

---
+++ lib/jobs.dart
@@ -0,0 +1,13 @@
+class Jobs {
+  String jobPosition;
+  String companyName;
+  String location;
+  String salaryRange;
+
+  Jobs({
+    this.jobPosition,
+    this.companyName,
+    this.location,
+    this.salaryRange
+  });
+}
