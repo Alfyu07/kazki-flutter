64f1d51e78e7344a935f406cee834942d8f17898
Gregorius Agung Narindra Aditantyo <narindra.aditantyo@gmail.com>
2020-09-02 17:44:10 +0700

    [DONE] All pages and ready for Dicoding submission

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final textController = TextEditingController();
   String displayName;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,13 +84,11 @@ class LoginPage extends StatelessWidget {
                         borderRadius: BorderRadius.circular(8.0)
                       ),
                       child: TextField(
+                        controller: textController,
                         decoration: InputDecoration(
                           border: InputBorder.none,
                           hintText: 'Input your name'
                         ),
-                        onChanged: (input) {
-                          displayName = input;
-                        },
                       ),
                     )
                   ],
@@ -94,13 +98,18 @@ class LoginPage extends StatelessWidget {
               FlatButton(
                 color: kOrange,
                 onPressed: () {
-                  if(displayName != null) {
+                  setState(() {
+                    displayName = textController.text;
+                  });
+
+                  if(displayName != '') {
                     Navigator.push(context, MaterialPageRoute(builder: (context) {
                       return HomePage(displayName: displayName);
                     }));
                   } else {
                     return null;
                   }
+                  
                 },
                 child: Text(
                   'NEXT',
@@ -268,7 +277,15 @@ class _HomePageState extends State<HomePage> {
                       itemBuilder: (context, index) {
                         return InkWell(
                           onTap: () {
-                            
+                            Navigator.push(context, MaterialPageRoute(builder: (context) {
+                              return JobsDetail(
+                                jobPosition: recommendedJobs[index].jobPosition,
+                                companyName: recommendedJobs[index].companyName,
+                                location: recommendedJobs[index].location,
+                                salaryRange: recommendedJobs[index].salaryRange,
+                                logoName: recommendedJobs[index].logoName,
+                              );
+                            }));
                           },
                           child: Container(
                             width: 10*kSafePadding,
@@ -361,10 +378,18 @@ class _HomePageState extends State<HomePage> {
                     itemBuilder: (context, index) {
                       return InkWell(
                         onTap: () {
-
+                          Navigator.push(context, MaterialPageRoute(builder: (context) {
+                            return JobsDetail(
+                              jobPosition: recentJobs[index].jobPosition,
+                              companyName: recentJobs[index].companyName,
+                              location: recentJobs[index].location,
+                              salaryRange: recentJobs[index].salaryRange,
+                              logoName: recentJobs[index].logoName,
+                            );
+                          }));
                         },
                         child: Container(
-                          margin: index != recentJobs.length-1 ? EdgeInsets.only(bottom: kSafePadding) : EdgeInsets.all(0),
+                          margin: EdgeInsets.only(bottom: kSafePadding),
                           padding: EdgeInsets.all(kSafePadding),
                           decoration: BoxDecoration(
                             border: Border.all(
@@ -443,6 +468,277 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class JobsDetail extends StatelessWidget {
+  final String jobPosition;
+  final String companyName;
+  final String location;
+  final String salaryRange;
+  final String logoName;
+  
+  JobsDetail({ this.jobPosition, this.companyName, this.location, this.salaryRange, this.logoName });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kLightGrey,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: kOrange
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          }
+        ),
+        centerTitle: true,
+        title: Text(
+          companyName,
+          style: GoogleFonts.poppins(
+            color: kBlack,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600
+          )
+        )
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          minimum: EdgeInsets.all(kSafePadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: kSafePadding),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/$logoName',
+                      scale: 1.5,
+                    ),
+                    SizedBox(width: kSafePadding),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          jobPosition,
+                          style: GoogleFonts.lato(
+                            color: kBlack,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w700
+                          )
+                        ),
+                        Text(
+                          companyName,
+                          style: GoogleFonts.lato(
+                            color: kDarkGrey,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w700
+                          )
+                        ),
+                        Text(
+                          location,
+                          style: GoogleFonts.lato(
+                            color: kDarkGrey,
+                            fontSize: 16.0
+                          )
+                        ),
+                        Text(
+                          salaryRange,
+                          style: GoogleFonts.lato(
+                            color: kDarkGrey,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600
+                          )
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 1.5*kSafePadding),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Job Description',
+                      style: GoogleFonts.poppins(
+                        color: kBlack,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700
+                      )
+                    ),
+                    SizedBox(height: kBasePadding),
+                    Text(
+                      'I am looking for a strong and efficient UI/UX designer to add to my team. Candidate must pay strong attention to Material.io guidelines. Client must also be proficient with the following technologies: Adobe XD, Invision, Photoshop, Illustrator, Zeplin.',
+                      textAlign: TextAlign.justify,
+                      style: GoogleFonts.lato(
+                        color: kBlack,
+                        fontSize: 16.0
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 1.5*kSafePadding),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Qualifications',
+                      style: GoogleFonts.poppins(
+                        color: kBlack,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700
+                      )
+                    ),
+                    SizedBox(height: kBasePadding),
+                    Text(
+                      '•\tMin level of education: B.Sc. in CSE',
+                      style: GoogleFonts.lato(
+                        color: kBlack,
+                        fontSize: 16.0
+                      )
+                    ),
+                    Text(
+                      '•\tExpert in Laravel framework',
+                      style: GoogleFonts.lato(
+                        color: kBlack,
+                        fontSize: 16.0
+                      )
+                    ),
+                    Text(
+                      '•\tFluent in English language',
+                      style: GoogleFonts.lato(
+                        color: kBlack,
+                        fontSize: 16.0
+                      )
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 1.5*kSafePadding),
+              Container(
+                padding: EdgeInsets.all(2*kSafePadding),
+                decoration: BoxDecoration(
+                  color: kLightGrey,
+                  borderRadius: BorderRadius.circular(8.0)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.people_outline,
+                          color: kOrange,
+                          size: 28.0,
+                        ),
+                        SizedBox(width: 1.5*kSafePadding),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Vacancy',
+                              style: GoogleFonts.lato(
+                                color: kBlack,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500
+                              )
+                            ),
+                            SizedBox(height: kBasePadding),
+                            Text(
+                              '03',
+                              style: GoogleFonts.lato(
+                                color: kBlack,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w700
+                              )
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(height: kSafePadding),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_today,
+                          color: kOrange,
+                          size: 28.0,
+                        ),
+                        SizedBox(width: 1.5*kSafePadding),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Application Deadline',
+                              style: GoogleFonts.lato(
+                                color: kBlack,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500
+                              )
+                            ),
+                            SizedBox(height: kBasePadding),
+                            Text(
+                              '31 Aug 2020',
+                              style: GoogleFonts.lato(
+                                color: kBlack,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w700
+                              )
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(height: kSafePadding),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.picture_in_picture_alt,
+                          color: kOrange,
+                          size: 28.0,
+                        ),
+                        SizedBox(width: 1.5*kSafePadding),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Interview',
+                              style: GoogleFonts.lato(
+                                color: kBlack,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500
+                              )
+                            ),
+                            SizedBox(height: kBasePadding),
+                            Text(
+                              '05 Sep 2020',
+                              style: GoogleFonts.lato(
+                                color: kBlack,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w700
+                              )
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
